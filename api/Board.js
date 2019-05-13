c1b7a212bb33800f066225d27ab37993e4abbe5f
ndb796 <ndb796@naver.com>
2019-05-13 15:27:34 +0900

    Add All Boards Read Module

--- api/Board.js
+++ api/Board.js
@@ -3,6 +3,32 @@ const router = express.Router();
 const Board = require('../models/Board');
 
 // Index 경로
+router.get('/',
+    // 오름차순으로 ID 값을 검색합니다.
+    function(req, res, next) {
+        let query = {};
+        // 대소문자를 무시하고 검색 조건을 명시합니다.
+        if(req.query.name) {
+            query.name = {$regex:req.query.name, $options:'i'};
+        }
+        if(req.query.content) {
+            query.content = {$regex:req.query.content, $options:'i'};
+        }
+        Board.find(query)
+        .sort({id: 1})
+        .exec(function(err, boards) {
+            if(err) {
+                res.status(500);
+                res.json({success: false, data: err})
+            }
+            else {
+                res.json({success: true, data: boards})
+            }
+        });
+    }
+)
+
+// Index 경로
 router.post('/',
     // 내림차순으로 최신 ID 값을 검색합니다.
     function(req, res, next) {
