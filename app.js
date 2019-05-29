aa29e30f0ba497c35d26da08f8e74aad322c2cae
ndb796 <ndb796@naver.com>
2019-05-30 08:53:51 +0900

    Allow Password Header

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'content-type');
+    res.header('Access-Control-Allow-Headers', 'content-type, password');
     next();
 });
 
